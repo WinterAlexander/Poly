99ea2a49ecfca51db75d3da7e6cc662c9616bf02
Winter Alexander <1541869@cegepmontpetit.ca>
2017-09-18 10:46:56 -0400

    Fixed test project

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,11 +1,5 @@
-#include "gtest/googletest/include/gtest/gtest.h"
 
-
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+#include "gtest/gtest.h"
 
 
 TEST(polynomial, test_add)
